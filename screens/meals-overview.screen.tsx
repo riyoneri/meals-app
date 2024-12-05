dd7ae23276ee0391723c26e934d7c4fd15c9ee12
Lionel Kaneza <lionkaneza@gmail.com>
2024-12-05 08:57:03 +0200

    refactor: display category id in route parameters

--- screens/meals-overview.screen.tsx
+++ screens/meals-overview.screen.tsx
@@ -1,9 +1,21 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function MealsOverviewScreen() {
+import { RootStackParameterList } from "../app";
+
+type MealsOverviewScreenProperties = NativeStackScreenProps<
+  RootStackParameterList,
+  "MealsOverview"
+>;
+
+export default function MealsOverviewScreen({
+  route,
+}: MealsOverviewScreenProperties) {
+  const categoryId = route.params.categoryId;
+
   return (
     <View style={styles.container}>
-      <Text>This is Category details</Text>
+      <Text>This is Category details - {categoryId}</Text>
     </View>
   );
 }
