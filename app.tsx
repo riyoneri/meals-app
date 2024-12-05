eb1e59737326f33f25684e5f38142a30ac807f3a
Lionel Kaneza <lionkaneza@gmail.com>
2024-12-05 08:52:26 +0200

    feat: setup react-navigation in the app

--- app.tsx
+++ app.tsx
@@ -1,15 +1,30 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import * as SystemUI from "expo-system-ui";
 
-import CategoriesScreen from "./screens/categories-screen";
+import CategoriesScreen from "./screens/categories.screen";
+import MealsOverviewScreen from "./screens/meals-overview.screen";
+
+export type RootStackParameterList = {
+  MealsCategories: undefined;
+  MealsOverview: { categoryId: string };
+};
 
 SystemUI.setBackgroundColorAsync("#24180f");
+
+const Stack = createNativeStackNavigator<RootStackParameterList>();
 
 export default function App() {
   return (
     <>
-      <StatusBar style="light" />
-      <CategoriesScreen />
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
