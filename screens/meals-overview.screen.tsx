03724439c75958c0d79ea8657a0344daeb141a43
Lionel Kaneza <lionkaneza@gmail.com>
2024-12-08 17:25:41 +0200

    refactor: extract meals from passed category id

--- screens/meals-overview.screen.tsx
+++ screens/meals-overview.screen.tsx
@@ -1,21 +1,36 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, ListRenderItemInfo, StyleSheet, View } from "react-native";
 
 import { RootStackParameterList } from "../app";
+import MealItem from "../components/meal-item";
+import { MEALS } from "../data/dummy-data";
+import Meal from "../models/meal";
 
 type MealsOverviewScreenProperties = NativeStackScreenProps<
   RootStackParameterList,
   "MealsOverview"
 >;
 
+function renderMealItem(mealData: ListRenderItemInfo<Meal>) {
+  return <MealItem {...mealData.item} />;
+}
+
 export default function MealsOverviewScreen({
   route,
 }: MealsOverviewScreenProperties) {
   const categoryId = route.params.categoryId;
 
+  const displayedMeals = MEALS.filter((mealItem) =>
+    mealItem.categoryIds.includes(categoryId),
+  );
+
   return (
     <View style={styles.container}>
-      <Text>This is Category details - {categoryId}</Text>
+      <FlatList
+        data={displayedMeals}
+        renderItem={renderMealItem}
+        keyExtractor={(item) => item.id}
+      />
     </View>
   );
 }
