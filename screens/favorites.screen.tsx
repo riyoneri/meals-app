921ece9d9f69d6490671fdd2af8e80b2e671244b
Lionel Kaneza <lionkaneza@gmail.com>
2024-12-13 09:28:14 +0200

    feat: create favorites screen

---
+++ screens/favorites.screen.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text } from "react-native";
+
+export default function FavoritesScreen() {
+  return <Text style={styles.text}>This is a favorite screen</Text>;
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: "white",
+  },
+});
